7868499ee11e1f513c3bbd337e0940a10df9810a
Drew Balducci <drewbalducci@gmail.com>
2026-04-29 13:03:18 -0500

    Initial Commit

---
+++ chess.cpp
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * *
+ *
+ * chess.cpp - Authored by Drew Balducci - Apr 29, 2026
+ *
+ * Handle logic of chess game from moves input externally using bitboards
+ * Intended to be interfaced later with python or Go/Rust server programs
+ *
+ * * * * * * * * * * * * * * * * * * * */
+
+
+int main()
+{
+
+	return 0;
+}
